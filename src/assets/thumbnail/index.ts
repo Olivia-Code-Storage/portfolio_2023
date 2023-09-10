95487493464d5704d63b9b001b896e2dd47065e3
hansololiviakim <dev.hansolkim@gmail.com>
2023-09-11 04:19:29 +0900

    :bento: Add thumbnail, ui element images

---
+++ src/assets/thumbnail/index.ts
@@ -0,0 +1,5 @@
+import belog from './belog.png'
+import dawhisky from './dawhisky.png'
+import diedie from './diedie.png'
+
+export { belog, dawhisky, diedie }
--- src/assets/ui/index.ts
+++ src/assets/ui/index.ts
@@ -1,5 +1,9 @@
-import background from './background.svg'
-import arrowDown from './arrowDown.png'
+import arrow from './arrow.png'
 import astronaut from './astronaut.png'
+import background from './background.svg'
+import github from './github.png'
+import notion from './notion.png'
+import shortcut from './shortcut.png'
+import tistory from './tistory.png'
 
-export { background, arrowDown, astronaut }
+export { arrow, astronaut, background, github, notion, shortcut, tistory }
