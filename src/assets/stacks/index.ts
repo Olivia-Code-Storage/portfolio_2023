0732314b66ce280db823bc220abf740f20620af7
hansololiviakim <dev.hansolkim@gmail.com>
2023-09-10 18:14:15 +0900

    :bento: Add stacks images

---
+++ src/assets/stacks/index.ts
@@ -0,0 +1,33 @@
+import axios from './axios.png'
+import css from './css.png'
+import figma from './figma.png'
+import html from './html.png'
+import javascript from './javascript.png'
+import notion from './notion.png'
+import react from './react.png'
+import reactquery from './reactquery.png'
+import recoil from './recoil.png'
+import redux from './redux.png'
+import slack from './slack.png'
+import styledcomponents from './styledcomponents.png'
+import typescript from './typescript.png'
+import vercel from './vercel.png'
+import vscode from './vscode.png'
+
+export {
+  axios,
+  css,
+  figma,
+  html,
+  javascript,
+  notion,
+  react,
+  reactquery,
+  recoil,
+  redux,
+  slack,
+  styledcomponents,
+  typescript,
+  vercel,
+  vscode,
+}
