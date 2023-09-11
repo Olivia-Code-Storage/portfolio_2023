93d1a6a53beebc29c9fb764803fca8d3667196d3
hansololiviakim <dev.hansolkim@gmail.com>
2023-09-11 21:25:55 +0900

    :sparkles: Add typing effcet

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,17 +1,32 @@
+import { useState, useEffect } from 'react'
 import { styled } from 'styled-components'
 import { arrow } from '../assets/ui'
 
 const Intro = () => {
+  const [currentWord, setCurrentWord] = useState('')
+  const [count, setCount] = useState(0)
+
+  const text = '안녕하세요!\nUX를 생각하는 개발자,\n김한솔입니다.'
+
+  useEffect(() => {
+    const typingWord = setInterval(() => {
+      if (count < text.length) {
+        setCurrentWord((prev) => prev + text[count])
+        setCount(count + 1)
+      } else {
+        clearInterval(typingWord)
+      }
+    }, 150)
+
+    return () => {
+      clearInterval(typingWord)
+    }
+  }, [count])
+
   return (
     <WrapIntro>
       <IntroduceDiv>
-        <p>{'안녕하세요!'}</p>
-        <div>
-          <span>{'[ '}</span>
-          <span>{''}</span>
-          <span>{' ]는 개발자,'}</span>
-        </div>
-        <p>{'김한솔입니다.'}</p>
+        <p>{currentWord}</p>
       </IntroduceDiv>
       <ScrollDiv>
         <div />
@@ -32,9 +47,19 @@ const WrapIntro = styled.section`
 const IntroduceDiv = styled.div`
   margin-left: 100px;
   margin-top: -50px;
-  display: flex;
-  flex-direction: column;
-  gap: 35px;
+  p {
+    font-size: 80px;
+    font-weight: 800;
+    line-height: 120px;
+    letter-spacing: 2px;
+    white-space: pre-line;
+  }
+  p::after {
+    content: '';
+    margin-left: 0.4rem;
+    border-right: 2px solid #666;
+    animation: cursor 0.9s infinite steps(2);
+  }
   &::before {
     content: '';
     width: 50px;
@@ -44,10 +69,13 @@ const IntroduceDiv = styled.div`
     left: 0;
     border-left: 10px solid ${({ theme }) => theme.ab};
   }
-  p,
-  span {
-    font-size: 80px;
-    font-weight: 800;
+  @keyframes cursor {
+    from {
+      border-right: 2px solid #222;
+    }
+    to {
+      border-right: 2px solid #666;
+    }
   }
 `
 
